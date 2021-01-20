eefa0b31c85b66020116dadc3db12d336cb889ca
Aleksei Slivka <alexprospero25@gmail.com>
2021-01-20 21:47:03 +0300

    Добавил ViewBinding

--- app/src/main/java/com/example/notesapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/ui/main/MainActivity.kt
@@ -2,20 +2,23 @@ package com.example.notesapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProviders
-import com.example.notesapp.R
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.notesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var ui: ActivityMainBinding
     lateinit var viewModel: MainViewModel
     lateinit var adapter: MainAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        ui = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+        setSupportActionBar(ui.toolbar)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         adapter = MainAdapter()
-        mainRecycler.adapter = adapter
+        ui.mainRecycler.adapter = adapter
         viewModel.viewState().observe(this, Observer<MainViewState> { t ->
             t?.let { adapter.notes = it.notes }
         })
